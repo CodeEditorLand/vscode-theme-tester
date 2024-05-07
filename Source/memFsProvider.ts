e3c498ec487313f69d06c7dd42f07787769ac3ab
Nikola Hristov <nikola@playform.cloud>
2024-05-07 15:38:56 +0300

    squash!

--- Source/memFsProvider.ts
+++ Source/memFsProvider.ts
@@ -81,6 +81,7 @@ export class MemFileSystemProvider implements FileSystemProvider {
 			entry.stats = stats;
 			entry.content = Promise.resolve(content);
 		}
+
 		this._fireSoon({ type: FileChangeType.Changed, uri });
 	}
 
